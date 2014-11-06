61a2b60b0d5a4cd81b49ad8661ef5029adc1949f
Gianna Fusaro <giannafusaro389@gmail.com>
2014-11-05 22:46:03 -0800

    write skeleton for repairs model

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,2 +1,26 @@
 class RepairsController < ApplicationController
+
+  def index
+    @repairs = Repair.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+private
+    def repairs_params
+      params.require(:repairs).permit(:customer_id, :employee_id, :computer_id, :description, :labor_cost, :total_cost, :status)
+    end
 end
