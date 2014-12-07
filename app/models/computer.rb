8a703d88a45fab7bd7b078856899d0a3646919b0
Gianna Fusaro <giannafusaro389@gmail.com>
2014-12-06 23:25:20 -0800

    Remove excess time evaluation stuff from computer model

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,17 +1,10 @@
 class Computer < ActiveRecord::Base
   belongs_to :customer
-
-  attr_accessor :offset
+  has_many :repairs
 
   def purchased_at=(str)
-    Rails.logger.debug "=========================================================="
-    Rails.logger.debug str
-    Rails.logger.debug DateTime.strptime(str, '%m/%d/%Y %I:%M %p')
-    Rails.logger.debug offset.inspect
-    Rails.logger.debug DateTime.strptime(str, '%m/%d/%Y %I:%M %p') + -(offset.to_i.hours)
-    Rails.logger.debug "=========================================================="
     if str.present?
-      self[:purchased_at] = DateTime.strptime(str, '%m/%d/%Y %I:%M %p') + -(offset.to_i.hours)
+      self[:purchased_at] = DateTime.strptime(str, '%m/%d/%Y %l:%M %p').change(offset: "-0800")
     end
   end
 end
