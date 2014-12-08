aca36233a8af313ea0afa8816c3c6d3950076fe9
Gianna Fusaro <giannafusaro389@gmail.com>
2014-12-07 17:14:26 -0800

    Fix update total cost trigger

--- db/migrate/20141207072714_add_trigger_to_update_total_cost.rb
+++ db/migrate/20141207072714_add_trigger_to_update_total_cost.rb
@@ -4,12 +4,17 @@ class AddTriggerToUpdateTotalCost < ActiveRecord::Migration
       CREATE TRIGGER update_cost AFTER UPDATE ON parts
       FOR EACH ROW
       BEGIN
-        DECLARE existing_total INT(11) DEFAULT 0;
+        DECLARE new_total INT(11) DEFAULT 0;
+        DROP TEMPORARY TABLE IF EXISTS t1;
+        CREATE TEMPORARY TABLE t1 (cost DECIMAL(8,2), quantity INT(11), total DECIMAL(8,2));
 
-        SELECT repairs.total_cost INTO existing_total FROM repairs
-        WHERE repairs.id = OLD.repair_id;
+        INSERT INTO t1
+          SELECT cost, quantity, (cost*quantity) FROM parts
+          WHERE repair_id = NEW.repair_id;
 
-        UPDATE repairs SET repairs.total_cost=(NEW.cost*NEW.quantity)
+        SELECT sum(total) INTO new_total FROM t1;
+
+        UPDATE repairs SET repairs.total_cost=new_total
         WHERE repairs.id = NEW.repair_id;
       END
     SQL
