61fdd648e79c6ec8c89ca3251201b8f426fcd113
Gianna Fusaro <giannafusaro389@gmail.com>
2014-12-11 17:25:36 -0800

    edit turnaround time function

--- newTurnAroundTime.sql
+++ newTurnAroundTime.sql
@@ -4,20 +4,22 @@ DROP PROCEDURE IF EXISTS average_tat;
 DELIMITER //
 CREATE PROCEDURE average_tat (OUT average FLOAT(4))
 BEGIN
-  DECLARE total_count FLOAT(4) DEFAULT 0;
-  DECLARE sum FLOAT(4) DEFAULT 0;
-  DECLARE temp FLOAT(4) DEFAULT 0;
-  DECLARE top INT(4) DEFAULT 0;
-  DECLARE bottom INT(4) DEFAULT 0;
-  SELECT count(*) INTO total_count
-  FROM repairs;
-  SELECT max(id) INTO top from repairs;
-  select min(id) into bottom from repairs;
-  WHILE bottom <= top DO
-  	select DATEDIFF(completed_at, created_at) FROM repairs
-  	WHERE id = bottom INTO temp;
-  	set sum = sum + temp;
-  END WHILE;
-  SET average = sum / total_count;
+
+DECLARE sum FLOAT(4) DEFAULT 0;
+DECLARE total_count FLOAT(4) DEFAULT 0;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS differences (
+  delta FLOAT(4) NOT NULL
+);
+
+INSERT INTO differences (delta)
+SELECT DATEDIFF(completed_at, requested_for)
+FROM repairs
+WHERE completed_at AND requested_for IS NOT NULL;
+
+SELECT avg(delta) INTO average
+FROM differences;
+
+
 END //
 DELIMITER ;
